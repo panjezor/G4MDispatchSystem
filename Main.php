5f02ed43f537ee9f9a911b8a98e91dd65ce0dbdb
panjezor <filipjancieslik@gmail.com>
2020-06-05 10:26:06 +0100

    Initial commit

---
+++ Main.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Main
+{
+    // Start the day and prepare what you have to.
+    public function startBatch(){
+
+    }
+
+    // Add a new order to the list for the appropriate courier and use the algorithm based on them.
+    public function addConsignment(){
+
+    }
+
+    // Finish the day, send all the batches to the appropriate couriers
+    public function endBatch(\http\Env\Request $request){
+
+    }
+}
